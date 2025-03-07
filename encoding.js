144b0404612d5e526615a2041fa95c85717e576b
Mr-Donot <florian.daunay37@gmail.com>
2025-03-07 21:48:10 +0100

    add: todo list for encoding

--- encoding.js
+++ encoding.js
@@ -14,12 +14,13 @@ async function encodeAndStoreMusic() {
     // Afficher la barre de progression
     showProgressBar();
 
-    // Étapes d'encodage
-    await updateProgress(10, "Vérification du lien...");
-    await updateProgress(30, "Téléchargement de l'audio...");
-    await updateProgress(60, "Conversion en empreinte sonore...");
-    await updateProgress(90, "Stockage des données...");
-    await updateProgress(100, "Encodage terminé !");
+    if (!(youtubeInput.value.includes("youtube.com/watch?v=") || youtubeInput.value.includes("youtu.be/"))) {
+        console.error("Lien YouTube invalide !");
+        return;
+    }
+
+    encodeAndStoreMusic(youtubeInput.value);
+
 
     // Attente + cacher la barre et réafficher les boutons
     await new Promise(resolve => setTimeout(resolve, 1200));
@@ -29,4 +30,59 @@ async function encodeAndStoreMusic() {
 
     console.log("Encodage terminé !");
     youtubeInput.value = "";
+}
+
+async function encodeAndStoreMusic(youtubeLink) {
+    // Étapes d'encodage
+    // await updateProgress(10, "Vérification du lien...");
+    // await updateProgress(30, "Téléchargement de l'audio...");
+    // await updateProgress(60, "Conversion en empreinte sonore...");
+    //await updateProgress(90, "Stockage des données...");
+    // await updateProgress(100, "Encodage terminé !");
+
+    let spectogram = getSpectogram(youtubeLink);
+    let anchorPoints = getAnchorPoints(spectogram);
+    let hashmap = getHashmap(anchorPoints);
+    storeHashmap(hashmap);
+
+}
+
+async function getSpectogram(youtubeLink) {
+    // get mp3 from youtube link
+
+    // transform mp3 to frequency data
+
+    // partition to optimize search
+
+
+    // hamming window to avoid spectral leakage
+
+    // FFT
+
+    // return spectogram --> list of partition
+}
+
+async function getAnchorPoints(spectogram) {
+
+    bands = [[0, 10], [10, 20], [20, 40], [40, 80], [80, 160], [160, 511]]
+
+    // for each partition in spectogram
+    //      take loudest of each band —> temporary points
+
+    // calc average of 6 strongest freq
+    //      if under, then discarded of point list
+
+    // return list of anchor points
+}
+
+async function getHashmap(anchorPoints) {
+    // for each anchor point i
+    //      take 5 anchor j in fixed target zone
+    //          hash pair (i, j)
+    // return hashmap
+}
+
+async function storeHashmap(hashmap) {
+    // store hashmap in firebase
+    // new entry is not exist, else add in existing entry
 }
